113a09a1d58000d221d63e12d5495d321e4c71a5
lekdel <alekdelisle@hotmail.com>
2024-03-20 20:28:51 -0400

    myscript change

--- myscript.py
+++ myscript.py
@@ -11,8 +11,8 @@ def run_bisect():
 
 # Main
 if __name__ == "__main__":
-    bad_revision = input("Entrez la révision mauvaise : ")
-    good_revision = input("Entrez la révision bonne : ")
+    bad_revision = "c1a4be04b972b6c17db242fc37752ad517c29402"
+    good_revision = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
 
     start_bisect(bad_revision, good_revision)
     run_bisect()
